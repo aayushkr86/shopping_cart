92539e18863d6b13432b3c4af91b9e8556218786
aayushkr86 <aayushkr10@gmail.com>
2018-05-24 12:47:23 +0530

    some changes

--- coupons/query.js
+++ coupons/query.js
@@ -1,5 +1,6 @@
 var Coupons = require('../models/couponmodel')
 var ObjectId = require('mongodb').ObjectID
+var Category = require('../models/categorymodel')
 
 // is coupon
 exports.iscoupon = function (req, param, next, callback) { //console.log(param.cart)
@@ -17,13 +18,37 @@ exports.iscoupon = function (req, param, next, callback) { //console.log(param.c
     }
 
     var flag = 0;
-    Object.keys(param.cart.totalitems).forEach(function(key) {
+    Object.values(param.cart.totalitems).forEach(function(key) { //console.log(key)
     // console.log(coupon.products.indexOf(key));
-    if(coupon.products.indexOf(key) != -1) {                     //check if product is present in coupon products array
+    if(coupon.products.indexOf(key.item._id) != -1) {                     //check if product is present in coupon products array
       flag = 1;
     }
     });
-    if(flag ==1 ) { 
+    // var temp = 0;
+    // function abc(){
+
+    //   Object.values(param.cart.totalitems).forEach(function(key) { //console.log(key)
+      
+    //   Category.findOne({"name" : key.item.category }).then(function(category){ console.log(category)
+    //   //  console.log(coupon.categories.indexOf(category._id));
+    //     if(coupon.categories.indexOf(category._id) != -1) {                     //check if product is present in coupon products array
+    //       // console.log('fuck youu')    
+    //        temp = 1;
+    //       console.log("1",temp) 
+      
+    //   }
+    //   console.log("2",temp)
+    //   })
+    //   console.log("3",temp)
+      
+    //   });
+    // }
+    // abc()
+    
+    //   console.log("4",temp)
+
+
+    if(flag == 1 ) { 
       return  callback(false , coupon)
     }else {
       callback(true ,"no coupon found")
